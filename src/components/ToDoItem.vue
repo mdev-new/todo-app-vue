6475b5d5e8032640f506733c794e306a6d0de19c
Mdev <121290257+mdev-new@users.noreply.github.com>
2024-06-11 21:43:44 +0200

    Initial commit

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+defineProps({
+  id: Number,
+  text: String,
+  done: Boolean
+})
+
+</script>
+
+<template>
+  <div class="flex flex-row gap-x-2">
+    <input type="checkbox" :id="id" :checked="done" class="hover:cursor-pointer" v-on:change="$emit('completed', id)"/>
+    <label :for="id">
+      <strike v-if="done">
+        {{ text }}
+      </strike>
+      <span v-else >
+        {{ text }}
+      </span>
+    </label>
+    <button class="border-2 hover:cursor-pointer hover:bg-red-400" v-on:click="$emit('deleted', id)">Smazat</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
